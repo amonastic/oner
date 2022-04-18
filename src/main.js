cafd08a07c19ffbb9d303725fee00cf7d847e9fc
Shane <17673621679@163.com>
2022-04-18 22:28:45 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
-const http = require('http'); //模块名字 = 导入模块（导入的模块名），可以使用模块名字使用node.js的http功能
+const { write } = require('fs');
+const http = require('http');
+const server = http.createServer((request, response) => {
 
-//requset：http模块会把请求相关的东西放在这个模块,缩写req
-//response：响应相关的东西，缩写res
-//两者名字无所谓，顺序很重要
+//给客户端响应的数据
+const data = {
+id:1,
+title:'关山月',
+}
 
-const server = http.createServer((request, Response) => {  //http模块提供的一个方法，把提供的服务放在一个函数中，该函数可以成为createServer方法的一个参数，可以单独定义或者直接添加无名的函数再交给createServer
-    console.log(request.headers['user-agent']); //请求request
+//转换成JSON格式
+const jsondata = JSON.stringify(data); //stringify转换JSON的方法，()写入转换的对象
 
-    request.write('hello ~');  //响应一段文字
-    Response.end(); //结束响应
+//因为是JSON，所哟告诉客户端这个数据是什么,（状态码200，对象）
+response.writeHead (200,{
+'content-type':'application/json; charset=utf-8'//头部数据，设置内容类型：类型；utf-8处理中文数据
+});
 
+//响应方法以响应jsondata
+response.write(jsondata);
+
+response.end(); //结束响应，不然客户端会卡主不动
+});
+
+server.listen(3000, () => { 
+console.log('🚀 服务已启动');
 }); 
-
-//设置服务器的监听
-server.listen(3000, () => {  //（端口号，函数）
-    console.log('🚀 服务已启动');
-});  //终端运行，一直在运行没有停止，control+C 停止服务
